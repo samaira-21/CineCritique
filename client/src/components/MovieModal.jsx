75b454ed2eaf4633bd77cf37415e44689fd40697
samaira-21 <samairaarora2103@gmail.com>
2026-01-01 17:54:38 +0530

    final changes commit

--- client/src/components/MovieModal.jsx
+++ client/src/components/MovieModal.jsx
@@ -15,15 +15,21 @@ export default function MovieModal({ movie, onClose }) {
   return (
     <div className="fixed inset-0 bg-white dark:bg-black bg-opacity-70 flex justify-center items-center z-50 backdrop-blur-[2px] transition-all duration-300">
       <motion.div
-        className="rounded-lg p-6 w-[90%] md:w-[600px] relative 
-        bg-[#FFFFFF] dark:bg-text-light
-        border border-[#E0E0E0] dark:border-[#2c2c2c]
-        text-[#1A1A1A] dark:text-gray-200
-        shadow-[0_0_20px_rgba(0,0,0,0.08)] dark:shadow-[0_0_20px_rgba(0,0,0,0.5)]
-        transition-colors duration-300"
+        className="rounded-lg p-6 w-[90%] md:w-[600px] relative
+max-h-[90vh]
+overflow-y-scroll
+[scrollbar-width:none] [-ms-overflow-style:none]
+[&::-webkit-scrollbar]:w-0
+bg-[#FFFFFF] dark:bg-text-light
+border border-[#E0E0E0] dark:border-[#2c2c2c]
+text-[#1A1A1A] dark:text-gray-200
+shadow-[0_0_20px_rgba(0,0,0,0.08)] dark:shadow-[0_0_20px_rgba(0,0,0,0.5)]
+transition-colors duration-300"
+
         initial={{ scale: 0.8, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
       >
+
         {/* Close Button */}
         <button
           onClick={onClose}
@@ -40,7 +46,7 @@ export default function MovieModal({ movie, onClose }) {
             className="w-32 h-48 object-cover rounded-md border border-[#E0E0E0] dark:border-[#2c2c2c]"
           />
           <div>
-            <h2 className="text-2xl font-bold text-[#A06CD5] dark:text-[#B8ACF6]">
+            <h2 className="text-2xl font-bold text-[#A06CD5] dark:text-[#A06CD5]">
               {movie.Title}
             </h2>
             <p className="text-gray-500 dark:text-gray-400">{movie.Year}</p>
